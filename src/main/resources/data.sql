6751ebb78216923bbaa7bdce97b3ab1c5c0d7e29
SebastianBoehrig <Sebastian.boehrig@gmail.com>
2026-02-01 20:18:55 +0100

    feat: added new default workstations to data.sql, removed old one

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,14 @@
 INSERT INTO `world_state` (`id`, `day`)
 VALUES (1, 0);
+
+INSERT INTO `workstation_type` (`name`)
+VALUES ('Farm'),
+       ('Tree farm'),
+       ('Mushroom farm'),
+       ('Pen'),
+       ('Hatchery'),
+       ('Brewery'),
+       ('Millstone'),
+       ('Oven'),
+       ('Stove'),
+       ('Station');
